5eb5e9d4b87ac98f1726e246bf33b51ea3b43bb9
Wyatt Johnson <wyatt.j1834@gmail.com>
2020-07-17 08:33:35 -0600

    Before voyager install

--- resources/views/emails/orderConfirmation.blade.php
+++ resources/views/emails/orderConfirmation.blade.php
@@ -10,7 +10,7 @@
 
         We'll ship your order to {{$shippingAddress}}.<br>
         You should recieve another email with tracking information when your order ships.<br>
-        If you have any questions, visit <a href="www.vintage-treasures-logan.com">our site</a> for contact information, or feel free to call us at <a href="tel:1-435-374-9436">(435) 374-9436</a>.
+        If you have any questions, visit <a href="https://www.vintage-treasures-logan.com">our site</a> for contact information, or feel free to call us at <a href="tel:1-435-374-9436">(435) 374-9436</a>.
     </p>
 </div>
 
